f0ea48de56d92318e9dfa14a1f2b08f09f24de19
HiTymek <wojcik.tymoteusz1999@gmail.com>
2022-07-21 22:06:59 +0200

    code formatted

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@
             ...tasks.slice(0, taskIndex),
             { ...tasks[taskIndex], done: !tasks[taskIndex].done },
             ...tasks.slice(taskIndex + 1),
-        ]
+        ];
         render();
     }
 
